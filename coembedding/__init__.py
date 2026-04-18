e8bf90e5f374656b1eb8cb455adbb335b5e1f6e8
hebutdy <98182524+hebutdy@users.noreply.github.com>
2026-04-18 20:48:58 +0800

    Add files via upload

---
+++ coembedding/__init__.py
@@ -0,0 +1,7 @@
+# from pkg_resources import get_distribution
+
+# __version__ = get_distribution('scalex').version
+__version__ = '1.0.0'
+__author__ = 'Ai Hanzhen'
+
+from .function import co_embedding
